1ee36ac74b8faa1a85040554818c4387d8b0210a
Monica Rangel <joanna.monica@yahoo.com>
2020-07-13 22:45:06 -0700

    adding index and license generate markdown JS files

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# ReadMe-Generator-
-# ReadMe-Generator
+
+  # Read Me Generator
+  undefined
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Test](#test)
+  - [License](#license)
+  - [Questions](#questions)
+  ## Description
+  This application
+  ## Installation
+  
+  ## Usage
+  
+  ## Contribution
+  
+  ## Test
+  undefined
+  ## License
+  undefined
+  ## Questions
+  - Github: https://github.com/undefined
+  - Email: 
+  
--- index.js
+++ index.js
@@ -1,14 +1,80 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkDown = require('./utils/generateMarkdown')
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // array of questions for user
-const questions = [
+const questions = ['What is the title of your project?', 'What is your project about?', 'What are your installation requirements?', 'How do use this application?', 'What license would you like to use?', 'Are you open to contributions?(If you are, please provide your requirements)', 'What command will run your test?', 'Enter your GitHub username.', 'Enter a valid email for your users to reach you.'];
 
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[0]
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[1]
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: questions[2]
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: questions[3]
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: questions[5]
+        },
+        {
+            type: 'input',
+            name: 'testInstructions',
+            message: questions[6]
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: questions[4],
+            choices: [
+                'Apache License 2.0',
+                'BSD 3-Clause',
+                'BSD 2-Clause',
+                'MIT'
+            ]
+        },
+        {
+            type: 'input',
+            name: 'githubUser',
+            message: questions[7]
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: questions[8]
+        }
+    ]);
+};
 
 // function to initialize program
-function init() {
+async function init() {
+    console.log('Hello!');
+    try {
+        const answers = await promptUser();
+        const readme = generateMarkDown(answers);
+        await writeFileAsync("README.md", readme);
+        console.log('Successfully Wrote README.md!');
+    } catch (err) {
+        console.log(err);
+    };
 
 }
 
